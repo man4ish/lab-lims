c05ce15264e998d7691ebf93fe5848a35e212bbc
Manish Kumar <manishkumar@mac.lan>
2025-05-26 13:28:39 -0500

    Refactor: Add initial tests setup in core app

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from core.models import Instrument
 
-# Create your tests here.
+class InstrumentModelTest(TestCase):
+    def test_instrument_creation(self):
+        instrument = Instrument.objects.create(label="Test Instrument", instrument_type_id=1) # Assuming InstrumentType exists with ID 1
+        self.assertEqual(instrument.label, "Test Instrument")
